a606cf17ccd16176a8a5e9914b6d996797327fd4
Jon Colverson <github@vcxz.co.uk>
2015-10-24 19:04:47 +0100

    Added option to wait for update to propagate to all Route53 servers.

--- route53dynamicdns.py
+++ route53dynamicdns.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
+import argparse
 import socket
 import sys
+import time
 
 import boto3
 
 def log(message): print(message)
 
-host_name = sys.argv[1]
+if len(sys.argv) == 1:
+	sys.argv.append('-h')
+
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument('--wait-for-route53-propagation', action='store_true',
+	help='wait for update to propagate to all Route53 servers before exiting')
+
+arg_parser.add_argument('host_name',
+	help='the host name for the entry to update')
+
+args = arg_parser.parse_args()
 
 # Get local IP address that is used for Internet access.
 # from: http://stackoverflow.com/a/166589/98286
@@ -18,29 +31,29 @@ s.close()
 
 r53 = boto3.client('route53')
 
-if not host_name.endswith('.'): host_name = host_name + '.'
+if not args.host_name.endswith('.'): args.host_name = args.host_name + '.'
 
-log('Looking for Route53 record for %s' % host_name)
+log('Looking for Route53 record for %s' % args.host_name)
 
 zones = r53.list_hosted_zones()
 
 zone_id = None
 
 for zone in zones['HostedZones']:
-	if host_name.endswith(zone['Name']):
+	if args.host_name.endswith(zone['Name']):
 		zone_id = zone['Id']
 		break
 
 if zone_id is None:
-	raise Exception('No Route53 zone found for %s' % host_name)
+	raise Exception('No Route53 zone found for %s' % args.host_name)
 
 log('Getting existing record\'s TTL')
 
 ttl = r53.list_resource_record_sets(
-		HostedZoneId=zone_id, StartRecordName=host_name, StartRecordType='A', MaxItems='1'
+		HostedZoneId=zone_id, StartRecordName=args.host_name, StartRecordType='A', MaxItems='1'
 	)['ResourceRecordSets'][0]['TTL']
 
-log('Setting %s to point to %s with TTL %d' % (host_name, ip, ttl))
+log('Setting %s to point to %s with TTL %d' % (args.host_name, ip, ttl))
 
 response = r53.change_resource_record_sets(
 	HostedZoneId = zone_id,
@@ -49,7 +62,7 @@ response = r53.change_resource_record_sets(
 			{
 				'Action': 'UPSERT',
 				'ResourceRecordSet': {
-					'Name': host_name,
+					'Name': args.host_name,
 					'Type': 'A',
 					'TTL': ttl,
 					'ResourceRecords' : [
@@ -61,3 +74,9 @@ response = r53.change_resource_record_sets(
 			},
 		]
 	})
+
+if args.wait_for_route53_propagation:
+	while response['ChangeInfo']['Status'] != 'INSYNC':
+		log('Waiting for DNS update to propagate')
+		time.sleep(15)
+		response = r53.get_change(Id=response['ChangeInfo']['Id'])
